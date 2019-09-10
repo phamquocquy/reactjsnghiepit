304ac23e6c7142dc6173fb169b844163450aaced
quypv <quypv1@nal.vn>
2019-09-10 09:15:37 +0700

    video 13

--- src/components/Reset.jsx
+++ src/components/Reset.jsx
@@ -3,9 +3,7 @@ import React, { Component } from "react";
 class Reset extends Component {
   render() {
     return (
-     
      <button type="button" class="btn btn-danger">button</button>
-     
     );
   }
 }
