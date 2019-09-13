8508cb4b42f7cf66dd3735f2dd6f8b4d1c3b255f
phamquocquy <phamquocquy1996@gmail.com>
2019-09-13 10:44:12 +0700

    video 15

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Products from "./components/Products";
-import Color from "./components/Color";
-import Content from "./components/Content";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="row">
-        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-          <Products />
-        </div>
-        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-          <Color />
-        </div>
-        <div>
-          <Content />
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  onHandleChange = e => {
+    var target = e.target;
+    var name = target.name;
+    var value = target.value;
+    console.log(target);
+    this.setState({
+      [name]: value
+    });
+  };
+
+  onHandleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="container-fluid">
+          <div className="panel panel-primary">
+            <div className="panel-heading">
+              <h3 className="panel-title">form</h3>
+            </div>
+            <div className="panel-body">
+              <form onSubmit={this.onHandleSubmit}>
+                <div className="form-group">
+                  <label>user name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="username"
+                    onChange={this.onHandleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>password</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="password"
+                    onChange={this.onHandleChange}
+                  />
+                </div>
+
+                <button type="submit" className="btn btn-default">
+                  lưu lại
+                </button>
+                <button type="button" className="btn btn-default">
+                  xóa trắng
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
